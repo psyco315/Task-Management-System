8bbd341644a6474e0036fcf67f9e2a6e0924e175
psyco315 <parth2004batman@gmail.com>
2025-08-04 20:26:25 +0530

    Fixing routes for production

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer]
+    }
+  },
   build: {
     outDir: 'dist',
   },
